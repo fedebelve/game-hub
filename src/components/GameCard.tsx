a3b0d287b3ad6951bf74b0ecb36bcb9d108d71cc
Federico Belvedere <federico.belvedere@gmail.com>
2024-02-26 22:15:23 -0300

    displating platform icons

--- src/components/GameCard.tsx
+++ src/components/GameCard.tsx
@@ -1,6 +1,7 @@
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
-import React from 'react'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
 import { Game } from '../hooks/useGames'
+import PlatformIconList from './PlatformIconList'
+
 
 interface Props{
     game: Game
@@ -14,6 +15,9 @@ function GameCard({ game }: Props) {
             <Heading fontSize={'2xl'}>
                 {game.name}
             </Heading>
+            {/* puedo hacerlo asi -> pero mejor desestructurar al objeto platform   {game.parent_platforms.map((platform) => <Text>{platform.platform.name}</Text>)} */}
+            {/* {game.parent_platforms.map(({platform}) => <Text>{platform.name}</Text>)} */}
+            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)} />
         </CardBody>
     </Card>
   )
---
+++ src/components/PlatformIconList.tsx
@@ -0,0 +1,35 @@
+import { HStack, Icon, Text } from '@chakra-ui/react'
+import React from 'react'
+import { Platform } from '../hooks/useGames'
+import { FaWindows, FaPlaystation, FaXbox, FaApple, FaLinux, FaAndroid } from 'react-icons/fa'
+import { MdPhoneIphone} from 'react-icons/md'
+import { SiNintendo } from 'react-icons/si'
+import {BsGlobe} from 'react-icons/bs'
+import { IconType } from 'react-icons'
+
+
+interface Props {
+    platforms: Platform[]
+}
+
+const PlatformIconList = ({platforms}: Props) => {
+    const iconMap: {[key: string]: IconType} = { //le digo que no se preocupe por la key, porque esos names van a cumplir la funcion
+        pc: FaWindows,
+        playstatition: FaPlaystation,
+        xbox: FaXbox,
+        nintendo: SiNintendo,
+        mac: FaApple,
+        linux: FaLinux,
+        ios: MdPhoneIphone,
+        web: BsGlobe,
+        android: FaAndroid
+    }
+
+    return (
+    <HStack marginY={1} >
+     {platforms.map((platform) => (<Icon as={iconMap[platform.slug]} color='gray.500'/> ))}
+    </HStack>
+  )
+}
+
+export default PlatformIconList
