db810b88a26411aa92902d92f1f3f32d6dd34f97
Federico Belvedere <federico.belvedere@gmail.com>
2024-02-28 11:32:01 -0300

    displaying genres

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -11,7 +11,7 @@ function GameGrid() {
   return (
     <>
     {error && <Text>{error}</Text>}
-    <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 5}} padding={10} spacing={10}>
+    <SimpleGrid columns={{sm:1, md: 2, lg: 3, xl: 5}} padding={10} spacing={3}>
         {isLoading && skeletons.map(skeleton =><GameCardContainer key={skeleton}> <GameCardSkeleton/> </GameCardContainer>)}
         {data.map(game => 
         <GameCardContainer key={game.id}> <GameCard game={game}/> </GameCardContainer>
